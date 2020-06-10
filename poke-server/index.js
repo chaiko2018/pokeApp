b3091c49fcdfb8205563d79e5074bc2cfb9691c0
tommy <afitamoon@gmail.com>
2020-06-10 22:39:39 +0900

    init apollo-server

---
+++ poke-server/index.js
@@ -0,0 +1,49 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Pokemon {
+    id: ID
+    number: String
+    name: String
+    maxHP: String
+    types: [String]
+    image: String
+  }
+
+  type Query {
+    books: [Book]
+    getPoke: Pokemon
+    getPokes: [Pokemon]
+  }
+`;
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling",
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
