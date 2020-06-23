3eb5f7a1c798969689e1dea4e02686d7e96a86b4
tommy <afitamoon@gmail.com>
2020-06-23 23:07:17 +0900

    fix component

--- poke-app/src/components/pracReducer.tsx
+++ poke-app/src/components/pracReducer.tsx
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-
+/*
 const correctValue = [1, 3, 4, 5, 2, 8, 7, 9, 6];
 
 enum ActionType {
@@ -7,24 +7,39 @@ enum ActionType {
   ACTION_FAILURE = "ACTION_FAILURE",
 }
 
+interface inputState {
+  inputValue: number[];
+}
+
+interface inputAction {
+  type: ActionType;
+  clickNumber?: number;
+}
+
+const initialState = {
+  inputValue: [],
+};
+
+const reducer: React.Reducer<inputState, inputAction> = (
+  state: inputState,
+  action: inputAction
+) => {
+  switch (action.type) {
+    case ActionType.ACTION_SUCCESS:
+      return [];
+    case ActionType.ACTION_FAILURE:
+      return [];
+    default:
+      return;
+  }
+};
+
 export default function PracReducer() {
-  const [inputValue, dispatch] = useReducer((state: any, action: any) => {
-    switch (action.type) {
-      case ActionType.ACTION_SUCCESS:
-        return [...state];
-      case ActionType.ACTION_FAILURE:
-        return [];
-      default:
-        return;
-    }
-  });
+  const [inputValue, dispatch] = useReducer(reducer, initialState);
 
   const handleOnClick = (num: any) => {
-    inputValue[inputValue.length] = num;
-
-    for (let index = 0; index < inputValue.length; index++) {
-      if (inputValue[index] !== correctValue[index]) {
-        dispatch({ type: "failure" });
+    for (let index = 0; index < 9; index++) {
+      if (!correctValue[index]) {
       }
     }
     return;
@@ -36,5 +51,47 @@ export default function PracReducer() {
         <button onClick={() => handleOnClick(index + 1)}>{index + 1}</button>
       ))}
     </div>
+  );
+}
+*/
+const initialState = { count: 0 };
+
+interface countState {
+  count: number;
+}
+
+interface countAction {
+  type: ActionType;
+}
+
+enum ActionType {
+  ACTION_INCREMENT = "ACTION_INCREMENT",
+  ACTION_DECREMENT = "ACTION_DECREMENT",
+}
+
+function reducer(state: countState, action: countAction) {
+  switch (action.type) {
+    case ActionType.ACTION_INCREMENT:
+      return { count: state.count + 1 };
+    case ActionType.ACTION_DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+export default function PracReducer() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: ActionType.ACTION_INCREMENT })}>
+        +
+      </button>
+      <button onClick={() => dispatch({ type: ActionType.ACTION_DECREMENT })}>
+        -
+      </button>
+    </>
   );
 }
